7f1c910c966e8f8286f8ef1a6c39db90eb818f39
Fronzen548 <0688612510dp@gmail.com>
2026-08-09 16:53:33 +0300

    Add footer section

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="mt-20 border-t border-white/10 pt-10 text-center text-slate-500">
+      <p>© 2026 Fronzen548. Всі права захищені.</p>
+    </footer>
+  )
+}
+
+export default Footer
